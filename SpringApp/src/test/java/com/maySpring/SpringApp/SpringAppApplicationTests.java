da02f445ef7e7ead11687047f3727f1fc7ba4b08
Emilek <emilek@hotmail.co.uk>
2020-05-21 13:49:58 +0100

    First Commit

---
+++ SpringApp/src/test/java/com/maySpring/SpringApp/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maySpring.SpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
